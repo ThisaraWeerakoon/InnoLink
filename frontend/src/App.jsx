d0ac47f24a4e22aa24797037a77d3f56bb2d9053
Ushari_Ranasinghe <82868052+ushariRanasinghe@users.noreply.github.com>
2024-10-09 23:51:15 +0530

    Pages and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
 import Layout from "./components/layout/Layout.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/auth/SignUpPage.jsx";
+import LoginPage from "./pages/auth/LoginPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
+import {Route, Routes} from 'react-router-dom';
 function App() {
 
-  return <Layout>Hello world</Layout>
+  return (<Layout>
+      <Routes>
+        <Route path='/' element = {<HomePage/>}/>
+        <Route path='/signup' element = {<SignUpPage/>}/>
+        <Route path='/login' element = {<LoginPage/>}/>
+        <Route path='/profile' element = {<ProfilePage/>}/>
+
+      </Routes>
+  </Layout>);
 }
 
 export default App
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,6 @@
+const HomePage = () => {
+    return <div>
+        <h1>Home Page</h1>
+    </div>
+}
+export default HomePage;
